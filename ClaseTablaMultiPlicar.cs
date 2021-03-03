1e9e99ee2fcdc9744276f9dc665da056063ff8ae
Psycho Thinker <paraquezada@gmail.com>
2021-03-03 16:37:36 -0400

    Implementando cambios.

---
+++ ClaseTablaMultiPlicar.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace package
+{
+    class ClaseTablaMultiplicar
+    {
+public void TablaMultiplicar()
+{
+   int tbl=0;
+while(tbl != -1)
+{
+   Console.WriteLine("Ingrese un numero: ");
+    tbl = Convert.ToInt32(Console.ReadLine());
+
+if(tbl==-1)
+{
+    continue;
+}
+Console.WriteLine("Tabla del "+tbl);
+for(int h = 1; h <= 12; h++){
+    int mp = tbl * h;
+
+Console.WriteLine(tbl+" X "+h+" = "+mp);
+        }
+    }  
+}
+
+
+    }
+
+}
